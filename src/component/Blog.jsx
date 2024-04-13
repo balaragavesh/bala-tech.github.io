c176b8eb445a9aaa8673d316030ed33520466559
balaragavesh <balaragavesh@gmail.com>
2024-04-13 17:26:46 +0530

    assets2pub

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -8,8 +8,8 @@ export default function Blog() {
     "Our journey in laptop sales and service spans a vast array of expertise and dedication. With years of immersion in the industry, we've cultivated an unparalleled understanding of laptops' intricacies, evolving trends, and customer needs. At the core of our service lies a dedication to personalized support, where every client receives tailored solutions and dependable assistance. Backed by years of industry experience, we guarantee top-notch products, transparent communication, and unwavering customer support.";
   const content2 =
     "Our commitment to customer satisfaction goes beyond mere transactions; it's an ongoing journey of understanding, exceeding expectations, and building lasting relationships. We take pride in offering personalized attention, ensuring that each client receives tailored solutions and unwavering support. From guiding customers through informed purchase decisions to swiftly resolving technical issues, we thrive on ensuring a seamless and gratifying experience. Our relentless pursuit of customer happiness drives us to provide transparent communication, reliable service, and a dedication to surpassing expectations.";
-  const src1 = "src/assets/expert.jpg";
-  const src2 = "src/assets/customer-satis.jpg";
+  const src1 = "/expert.jpg";
+  const src2 = "/customer-satis.jpg";
   const data = [
     {
       h1: "10+",
